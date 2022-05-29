09230658595bf0519aaed6cd88a753de2752c114
VJS <minsung_96@naver.com>
2022-05-29 23:39:03 +0900

    Sidebar & useStateContext Hook

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css'
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages'
+
+import { useStateContext } from './contexts/ContextProvider'
 
 const App = () => {
-    const [activeMenu, setActiveMenu] = useState(true)
+    const { activeMenu } = useStateContext()
     
     return (
         <div>
@@ -47,8 +49,8 @@ const App = () => {
                     <div>
                         <Routes>
                             {/* Dashboard */}
-                            <Route path="/" element={<ECommerce />} />
-                            <Route path="/ecommerce" element={<ECommerce />} />
+                            <Route path="/" element={<Ecommerce />} />
+                            <Route path="/ecommerce" element={<Ecommerce />} />
                             {/* Pages */}
                             <Route path="/orders" element={<Orders />} />
                             <Route path="/employees" element={<Employees />} />
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './contexts/ContextProvider'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>
+);
 
 reportWebVitals();
