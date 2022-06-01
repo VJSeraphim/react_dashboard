536bff230dc9070dede0d59e28f25d525adc86bf
VJS <minsung_96@naver.com>
2022-06-01 23:53:22 +0900

    Navbar Toggle & Mobile Responsibilities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,24 @@ const NavButton = ({ title, customFnc, icon, color, dotColor }) => (
 )
 
 const Navbar = () => {
-    const [activeMenu, setActiveMenu] = useStateContext()
+    const [activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize] = useStateContext()
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth)
+        window.addEventListener('resize', handleResize)
+        handleResize()
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+      if (screenSize <= 900) {
+          setActiveMenu(false)
+      } else {
+          setActiveMenu(true)
+      }
+    }, [screenSize])
+    
 
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
@@ -68,6 +85,11 @@ const Navbar = () => {
                         <MdKeyboardArrowDown className="text-gray-400 text-14"/>
                     </div>
                 </TooltipComponent>
+
+                {isClicked.cart && <Cart />}
+                {isClicked.chat && <Chat />}
+                {isClicked.notification && <Notification />}
+                {isClicked.userProfile && <UserProfile />}
             </div>
         </div>
         
