2bed58a512bad5c35eb23cea38ba86502d4e97e6
VJS <minsung_96@naver.com>
2022-06-08 23:59:42 +0900

    Div Indentation & Stacked Chart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -78,37 +78,53 @@ const Ecommerce = () => {
                     </div>
                     <div className="mt-10 flex gap-10 flex-wrap justify-center">
                         <div className="border-r-1 border-color m-4 pr-10">
-                            <p>
-                                <span className="text-3xl font-semibold">
-                                    RANDOM DOLLAR HERE
-                                </span>
-                                <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
-                                    23%
-                                </span>
-                            </p>
-                            <p className="text-gray-500 mt-1">
-                                Budget
-                            </p>
+                            <div>
+                                <p>
+                                    <span className="text-3xl font-semibold">
+                                        RANDOM DOLLAR HERE
+                                    </span>
+                                    <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                                        23%
+                                    </span>
+                                </p>
+                                <p className="text-gray-500 mt-1">
+                                    Budget
+                                </p>
+                            </div>
+                            <div className="mt-8">
+                                <p>
+                                    <span className="text-3xl font-semibold">
+                                        RANDOM DOLLAR HERE
+                                    </span>
+                                </p>
+                                <p className="text-gray-500 mt-1">
+                                    Expense
+                                </p>
+                            </div>
+                            <div className="mt-5">
+                                <SparkLine 
+                                    currentColor="blue"
+                                    id="line-sparkline"
+                                    type="Line"
+                                    height="80px"
+                                    width="250px"
+                                    data={SparkLineAreaData}
+                                    color="blue"
+                                />
+                            </div>
+                            <div className="mt-10">
+                                <Button 
+                                    color="white"
+                                    bgColor="blue"
+                                    text="Download Report"
+                                    borderRadius="10px"
+                                />
+                            </div>
                         </div>
-                        <div className="mt-8">
-                            <p>
-                                <span className="text-3xl font-semibold">
-                                    RANDOM DOLLAR HERE
-                                </span>
-                            </p>
-                            <p className="text-gray-500 mt-1">
-                                Expense
-                            </p>
-                        </div>
-                        <div className="mt-5">
-                            <SparkLine 
-                                currentColor="blue"
-                                id="line-sparkline"
-                                type="Line"
-                                height="80px"
-                                width="250px"
-                                data={SparkLineAreaData}
-                                color="blue"
+                        <div>
+                            <Stacked 
+                                width="320px"
+                                height="360px"
                             />
                         </div>
                     </div>
