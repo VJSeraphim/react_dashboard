c0b8bbd5886085c9a1eb41b47481c5b6b48ec512
VJS <minsung_96@naver.com>
2022-06-11 23:56:17 +0900

    Calendar, Kanban

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMEnu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
-import { ordersData, contextMenuItems, ordersGrid } from '../assets/dummy'
+import { GridComponent, ColumnsDirective, ColumnDirective,  Page, Inject, Toolbar } from '@syncfusion/ej2-react-grids'
+import { employeesData, contextMenuItems, employeesGrid } from '../assets/dummy'
 import { Header } from '../components'
 import { ColumnDirective } from '@syncfusion/ej2-react-charts'
 
@@ -8,22 +8,24 @@ import { ColumnDirective } from '@syncfusion/ej2-react-charts'
 const Employees = () => {
     return (
         <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-            <Header title="Orders" category="page"/>
+            <Header title="Employees" category="Page"/>
             <GridComponent
                 id="gridcomp"
-                dataSource={ordersData}
+                dataSource={employeesData}
                 allowPaging
                 allowSorting
+                toolbar={['Search']}
+                width="auto"
             >
                 <ColumnsDirective>
-                    {ordersGrid.map((item, i) => (
+                    {employeesGrid.map((item, i) => (
                         <ColumnDirective 
                             key={i} 
                             {...item}
                         />
                     ))}
                 </ColumnsDirective>
-                <Inject services={[Resize, Sort, Filter, Page, ExcelExport, Edit, PdfExport]}/>
+                <Inject services={[Page, Search, Toolbar]}/>
             </GridComponent>
         </div>
     )
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMEnu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
 import { ordersData, contextMenuItems, ordersGrid } from '../assets/dummy'
 import { Header } from '../components'
 import { ColumnDirective } from '@syncfusion/ej2-react-charts'
@@ -8,7 +8,7 @@ import { ColumnDirective } from '@syncfusion/ej2-react-charts'
 const Orders = () => {
     return (
         <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-            <Header title="Orders" category="page"/>
+            <Header title="Orders" category="Page"/>
             <GridComponent
                 id="gridcomp"
                 dataSource={ordersData}
