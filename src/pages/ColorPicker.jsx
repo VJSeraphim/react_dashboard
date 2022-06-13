76181afcc622043fb590c96a7816ffd6d39b71cc
VJS <minsung_96@naver.com>
2022-06-13 23:54:27 +0900

    Editor & Color Picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
 
 const ColorPicker = () => {
     return (
-        <div>
-            ColorPicker
+        <div className="m-2 md:m:10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header 
+                category="App"
+                title="Color Picker"
+            />
+            <div className="text-center">
+                <div id="preview"/>
+                <div className="flex justify-center items-center gap-20 flex-wrap">
+                    <div>
+                        <p className="text-2xl font-semibold mt-2 mb-4">
+                            Inline Palette
+                        </p>
+                        <ColorPickerComponent 
+                            mode="Palette"
+                            modeSwitcher={false}
+                            id="inline-palette"
+                            inline
+                            showButtons={false}
+                        />
+                    </div>
+                    <div>
+                        <p className="text-2xl font-semibold mt-2 mb-4">
+                            Inline Picker
+                        </p>
+                        <ColorPickerComponent 
+                            mode="Picker"
+                            modeSwitcher={false}
+                            id="inline-picker"
+                            inline
+                            showButtons={false}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
